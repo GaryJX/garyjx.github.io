28c17e0bed910c3bd0979ba8c71861aa0992134c
Vasiariah <47544104+Vasiariah@users.noreply.github.com>
2019-06-26 21:46:47 -0400

    Added Scroll Reveal

---
+++ scrollReveal.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.scroll-left',{
+  duration: 1500,
+  origin: 'left',
+  distance: '200px'
+});
+
+ScrollReveal().reveal('.scroll-right', {
+  duration: 1500,
+  origin: 'right',
+  distance: '200px'
+});
